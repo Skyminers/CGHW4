51853832183c0b0d2995ceba0bacabbb130756be
Sky_miner <458482582@qq.com>
2020-10-25 03:53:24 +0800

    Complete a simple solar viewer.

--- src/main.cpp
+++ src/main.cpp
@@ -3,50 +3,32 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include "Shaders.h"
+#include "Camera.h"
+#include "Sphere.h"
 #include <iostream>
 
 //#define GLFW_DLL
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* windows, double xOffset, double yOffset);
+int init_set();
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char *vertexShaderSource =
-        "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "layout (location = 1) in vec3 aColor;\n"
-        "\n"
-        "out vec3 ourColor;\n"
-        "\n"
-        "uniform mat4 transform;\n"
-        "\n"
-        "void main() {\n"
-        "    gl_Position = transform * vec4(aPos, 1);\n"
-        "    ourColor = aColor;\n"
-        "}\0";
-const char *fragmentShaderSource =
-        "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "in vec3 ourColor;\n"
-        "\n"
-        "void main() {\n"
-        "    FragColor = vec4(ourColor, 1.0);//vec4(1.0, 0.5, 0.2, 1.0);\n"
-        "}\0";
+const float aroundTime1 = 10.0f;
+const float aroundTime2 = 8.0f;
+const float aroundTime3 = 2.0f;
+const float selfAroundTime = 2.0f;
 
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 
-float vertices[] = {
-         0.5, -0.5, 0 , 1.0, 0.0, 0.0,
-         -0.5, -0.5, 0, 0.0, 1.0, 0.0,
-         0.0,  0.5, 0 , 0.0, 0.0, 1.0
-};
-
-unsigned int indices[] = {
-        0, 1, 2
-};
-
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
 int main()
 {
@@ -72,6 +54,8 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
@@ -81,44 +65,14 @@ int main()
         return -1;
     }
 
-    // compile and link Shaders
-    // ---------------------------------------
-    int success;
-    char infoLog[512];
-    unsigned int shaderProgram = glCreateProgram();
+    glEnable(GL_DEPTH_TEST);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-    // vertex shader compile and link
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if(!success){
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR : when compiling vertexShader : " << infoLog << std::endl;
-        return -1;
-    }
-    glAttachShader(shaderProgram, vertexShader);
+    Shaders shader("../src/shaders/vertexShader.glsl", "../src/shaders/fragmentShader.glsl");
 
-    // fragment shader compile and link
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if(!success){
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR : when compiling fragmentShader : " << infoLog << std::endl;
-        return -1;
-    }
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if(!success){
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR : when compiling fragmentShader : " << infoLog << std::endl;
-        return -1;
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    Sphere sun(0.4f);
+    std::cout << sun.getIndicesSize() << std::endl;
+    std::cout << *(sun.getIndicesAddress()+1) << std::endl;
 
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
@@ -127,10 +81,10 @@ int main()
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sun.getVertexSize()*sizeof(float ), sun.getVertexAddress(), GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sun.getIndicesSize()*sizeof(int ), sun.getIndicesAddress(), GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), 0);
     glEnableVertexAttribArray(0);
@@ -141,11 +95,25 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-
     // render loop
     // -----------
+    shader.useProgram();
+
+    glm::mat4 rotate1;
+    glm::mat4 rotate2;
+    glm::mat4 rotateSate;
+    glm::mat4 selfRotation;
+
+    glm::vec3 posSp2(-0.8f, -1.3f, -0.9f);
+    glm::vec3 axisSp2 = glm::cross(posSp2, glm::vec3(0, 0, 1.0f));
+    glm::vec3 posSate(0.6f, 0.7f, 0.9f);
+    glm::vec3 axisSate = glm::cross(posSate, glm::vec3(0,0,1.0f));
+
     while (!glfwWindowShouldClose(window))
     {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
         // input
         // -----
         processInput(window);
@@ -153,20 +121,59 @@ int main()
         // render
         // ------
         glClearColor(0.2, 0.3, 0.3, 1.0);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        selfRotation = glm::rotate(selfRotation, glm::radians(deltaTime/selfAroundTime*360), glm::vec3(0.2f, 0.1f, 0.4f));
         // draw
-        glUseProgram(shaderProgram);
-        glm::mat4 trans = glm::mat4(1.0f);
-        glm::vec4 vec(1.0f,1.0f,0.0f,1.0f);
-        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
-        trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
-        vec = trans * vec;
-        unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+//        for(unsigned int i = 0;i < 10; ++ i) {
+//            glm::mat4 model;
+//            model = glm::translate(model, cubePositions[i]);
+//            model = glm::rotate(model, glm::radians((float)(20*(i+1)*glfwGetTime())),glm::vec3(0.5f, 0.7f, 0.6f));
+//            shader.setMat4("model", model);
+//            glDrawArrays(GL_TRIANGLES, 0, 36);
+//        }
 
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glm::mat4 projection = glm::perspective(glm::radians(camera.zoom), (float)SCR_WIDTH/SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.getViewMat();
+        glm::mat4 model;
+        model = model * selfRotation;
+        shader.setMat4("view", view);
+        shader.setMat4("projection", projection);
+        shader.setMat4("model", model);
+        shader.setBool("sun", true);
+
+        glDrawElements(GL_TRIANGLES, sun.getIndicesSize(), GL_UNSIGNED_INT, 0);
+
+        shader.setBool("sun", false);
+
+        rotate1 = glm::rotate(rotate1, glm::radians(deltaTime/aroundTime1*360), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = rotate1;
+        model = glm::translate(model, glm::vec3(3.0f, 0, 0));
+        model = model * selfRotation;
+        shader.setMat4("model", model);
+
+        glDrawElements(GL_TRIANGLES, sun.getIndicesSize(), GL_UNSIGNED_INT, 0);
+
+        model = rotate1;
+        model = glm::translate(model, glm::vec3(3.0f,0,0));
+        rotateSate = glm::rotate(rotateSate, glm::radians(deltaTime/aroundTime3*360), axisSate);
+        model = model * rotateSate;
+        model = glm::translate(model, posSate);
+        model = model * selfRotation;
+        model = glm::scale(model, glm::vec3(0.3f, 0.3f, 0.3f));
+        shader.setMat4("model", model);
+
+        glDrawElements(GL_TRIANGLES, sun.getIndicesSize(), GL_UNSIGNED_INT, 0);
+
+        rotate2 = glm::rotate(rotate2, glm::radians(deltaTime/aroundTime2*360), axisSp2);
+        model = rotate2;
+        model = glm::translate(model, posSp2);
+        model = model * selfRotation;
+        shader.setMat4("model", model);
+
+        glDrawElements(GL_TRIANGLES, sun.getIndicesSize(), GL_UNSIGNED_INT, 0);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -177,7 +184,6 @@ int main()
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
-    glDeleteProgram(shaderProgram);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
@@ -191,12 +197,40 @@ void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.processKey(cameraMovement::FORWARD, deltaTime);
+    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.processKey(cameraMovement::BACKWARD, deltaTime);
+    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.processKey(cameraMovement::LEFT, deltaTime);
+    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.processKey(cameraMovement::RIGHT, deltaTime);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos){
+    static bool firstFlag = true;
+    static float lastX, lastY;
+    if(firstFlag){
+        lastX = xpos;
+        lastY = ypos;
+        firstFlag = false;
+    }
+    float xOffset = (xpos - lastX);
+    float yOffset = (lastY - ypos);
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.processMouse(xOffset, yOffset);
+}
+
+void scroll_callback(GLFWwindow* windows, double xOffset, double yOffset){
+    camera.processScroll(yOffset);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     // make sure the viewport matches the new window dimensions; note that width and
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
--- src/shaders/vertexShader.glsl
+++ src/shaders/vertexShader.glsl
@@ -4,9 +4,13 @@ layout (location = 1) in vec3 aColor;
 
 out vec3 ourColor;
 
-uniform mat4 transform;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+uniform bool sun;
 
 void main() {
-    gl_Position = transform * vec4(aPos, 1);
-    ourColor = aColor;
+    gl_Position = projection * view * model * vec4(aPos, 1);
+    if(sun) ourColor = vec3(0.5f, 1.0f, 0);
+    else ourColor = aColor;
 }
